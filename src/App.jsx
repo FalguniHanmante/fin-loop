52e0badcf2adce22d389341e7b2c0ab81f3cec3a
Falguni Hanmante <fhanmante@gmail.com>
2026-07-07 09:44:15 +0530

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { Routes, Route } from "react-router";
+import Login from "./pages/login/Login";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Profile from "./pages/profile/Profile";
+import Signup from "./pages/signup/Signup";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="m-[10px] text-2xl font-bold text-emerald-800">
+      {/* <div className="m-[10px] text-2xl font-bold text-emerald-800">
         FinLoop App
-      </div>
+      </div> */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Profile />} />
+        <Route path="/" element={<Signup />} />
+      </Routes>
     </>
   );
 }
